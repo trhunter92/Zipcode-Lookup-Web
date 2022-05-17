a64870659188184792541877b629ff6f271ac114
Tyler Hunter <trhunter92@gmail.com>
2022-05-17 16:01:37 -0600

    initialized react app with typescript

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Thing } from "./Components/Thing"
+
+function App() {
+  return (
+    <Thing/>
+  );
+}
+
+export default App;
---
+++ src/Components/Thing.tsx
@@ -0,0 +1,5 @@
+export const Thing = () => {
+  return (
+    <div>Howdy ho neighbor</div>
+  )
+}
